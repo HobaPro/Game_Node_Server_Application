d4fde2db78f756f97746bf7025177162c2e7b8de
HobaPro <HobaProNum1@gmail.com>
2023-08-08 16:16:44 +0300

    CommitPro_20

--- api/routes/main.route.js
+++ api/routes/main.route.js
@@ -6,7 +6,7 @@ const authRoute = require("./auth.route");
 const progressRoute = require("./progress.route");
 
 router.get("/test", (req, res) => {
-    res.status(200).send("Yes I am Working..");
+    res.status(200).send("Yes I am Working_2");
 })
 
 router.get("/api/connect", (req, res) => {
